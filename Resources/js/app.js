f2a03354d1979ae3d2b910535c2bf2fbf94a8062
whroman <me@walterroman.com>
2014-08-19 12:10:25 -0700

    Refactored templates to work off js strings, rather than jquery selections of dom elements

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,69 +1,110 @@
 $(function() {
-    var info = {
-        $panel: $("#info"),
-        $secondary: $("#secondary"),
-        $org: $("#org"),
-        $totalDead: $("#total-dead"),
-        $type: $("#type"),
-        $typeDead: $("#type-dead"),
-        classHidden: "hidden",
-        updatePrimary: function(node) {
-            info.$org.html(node.title + "<span class='superscript'>" + node.sources + "</span>");
-            info.$totalDead.text(node.value);   
-        },
-        update: function(d) {
-            info.$panel.removeClass(info.classHidden);
 
-            if (d.depth === 1) {
-                info.$secondary.addClass(info.classHidden);
-                info.updatePrimary(d);
-            } else if (d.depth === 2) {
-                info.updatePrimary(d.parent);
-                info.$secondary.removeClass(info.classHidden);
-                info.$type.text(d.title);
-                info.$typeDead.text(d.value);              
-            }
+    var Templates = {
+        renderPrimaryTitle: function(node) {
+            var html = node.title + "<span class='superscript'>" + node.sources + "</span>";
+            return html;
         },
-        hide: function() {
-            info.$panel.addClass(info.classHidden);
+        renderPrimary: function(node) {
+            var html = "" +
+            "<div primary>" +
+                "<div class='text-lg'>" + Templates.renderPrimaryTitle(node) + "</div>" +
+                "<div class='spacer-xs'></div>" +
+                "<div class='text-md'> " +
+                    node.value + " Total Palestinians killed" +
+                "</div>" +
+            "</div>";
+
+            return html;
+        },
+        renderSecondary: function(node) {
+            var html = "" +
+                "<div secondary class='text-sm'>" +
+                    "<div>Including <span>" + node.value + "</span> <span>" + node.title + "</span> deaths</div>" +
+                "</div>";   
+            return html;
+        },
+        renderSource: function(source) {
+            var sourceRef = "<span>[" + (source.i + 1) + "] </span>";
+            var lastUpdated = "<span> - Retrieved " + source.lastUpdated + "</span";
+            var link = "<div><div class='spacer-xs'></div><a href='" + source.link + "'>" + source.link + "</a></div>";
+            var html = "<li class='text-xs'>" + sourceRef + lastUpdated + link + "<div class='spacer-md'></div></li>";
+
+            return html;
+        },
+        renderSourcesPanel: function($panel, sources) {
+            var i = 0,
+                sourcesLen = sources.length;
+
+            for ( i; i < sourcesLen; i++) {
+                var source = sources[i];
+                source.i = i;
+                var html = Templates.renderSource(source);
+
+                $panel.append(html);
+            }
         }
     };
 
-    var d3ui = {
-        onPathMouseover: function(d, paths) {
-
-            paths
+    var events = {
+        onPathMouseover: function(d, graph) {
+            graph.paths
             // Fade all fill colors
             .classed("faded", true)
             // Highlight only those that are an ancestor of the current segment
             .filter(function(node) {
-                var ancestors = d3ui.getAncestors(node, d);
+                var ancestors = D3Partitions.getNodeAncestors(node, d);
                 return ancestors;
             })
             .classed("faded", false);
 
-            info.update(d);
-        },
-        getAncestors: function(node, currentNode) {
-            var ancestors = [];
-            var whileNode = node;
-            while (whileNode.parent) {
-                ancestors.unshift(whileNode);
-                whileNode = whileNode.parent;
-            }
-            return (ancestors.indexOf(currentNode) >= 0);
-        },
-
+            graph.info.update(d);
+        }
     };
 
+    function InfoPanel ($wrapper) {
+        var $panel = $wrapper.find("[info]"),
+            $primary = $wrapper.find("[primary]"),
+            $secondary = $wrapper.find("[secondary]"),
+            classHidden = "hidden";
 
 
+        function updatePrimary(node) {
+            var html = Templates.renderPrimary(node);
+            $primary.html(html);  
+        }
 
-    var d3Partitions = function() {
+        function updateSecondary(node) {
+            var html = Templates.renderSecondary(node);
+            $secondary
+                .removeClass(classHidden)
+                .html(html);
+        }
+        
+        function update(d) {
+            if (d.depth === 1) {
+                $secondary.addClass(classHidden);
+                updatePrimary(d);
+            } else if (d.depth === 2) {
+                updatePrimary(d.parent);
+                updateSecondary(d);           
+            }
+        }
+
+        function hide() {
+            $panel.addClass(classHidden);
+        }
+
+        return {
+            update: update
+        };
+    }
+
+    var D3Partitions = function() {
         var width = 600,
             height = 600,
             radius = Math.min(width, height) / 2,
-            svgTranslateToCenter = "translate(" + width / 2 + "," + height / 2 + ")";
+            svgCenter = "translate(" + width / 2 + "," + height / 2 + ")";
 
         var partition = d3.layout.partition()
             .size([2 * Math.PI, radius * radius]);
@@ -74,9 +115,16 @@ $(function() {
             .innerRadius(function(d) { return Math.sqrt(d.y); })
             .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
 
-        function jsonRequestCB(error, json) {
-            var paths = svg.datum(json).selectAll("path")
-                .data(d3Partitions.partition.nodes)
+        function jsonRequestCB(error, json, graph) {
+            if (!error) {
+                renderGraph(json, graph);
+                Templates.renderSourcesPanel(graph.$sources, json.sources);                
+            }
+        }
+
+        function renderGraph(json, graph) {
+            graph.paths = graph.svg.datum(json).selectAll("path")
+                .data(D3Partitions.partition.nodes)
                 .enter()
                 .append("path")
                 .attr("display", function(d) {
@@ -85,12 +133,10 @@ $(function() {
                 .attr("class", function(d) { 
                     return d.title; 
                 })
-                .attr("d", d3Partitions.arc)
+                .attr("d", D3Partitions.arc)
                 .on("mouseover", function(d) {
-                    d3ui.onPathMouseover(d, paths);
-                });
-
-            renderSourcesPanel(".sources[for='casualties']", json.sources);
+                    events.onPathMouseover(d, graph);
+                });    
         }
 
         function render() {
@@ -99,49 +145,45 @@ $(function() {
                 elementsLen = elements.length;
 
             for ( i; i < elementsLen; i++ ) {
-                var element = d3.select(elements[i]),
-                    jsonPath = element.attr("data-source");
+                var element = d3.select(elements[i]);
+                var graph = {
+                    wrapper: element,
+                    jsonPath: element.attr("data-source"),
+                    svg: element.select("[graph]")
+                        .append("svg")
+                        .attr("width", width)
+                        .attr("height", height)
+                        .append("g")
+                        .attr("transform", svgCenter),
+                    $sources: $(elements[i]).find("[sources]"),
+                    info: new InfoPanel($(elements[i]))
+                };
 
-                svg = element.append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .append("g")
-                    .attr("transform", d3Partitions.centerSVG);
-
-
-                d3.json(jsonPath, d3Partitions.jsonRequestCB);
+                d3.json(graph.jsonPath, function(error, json) {
+                    D3Partitions.jsonRequestCB(error, json, graph);
+                });
             }
         }
 
-        function centerSVG() {
-            var translate =  "translate(" + width / 2 + "," + height / 2 + ")";
-            return translate;
+        function getNodeAncestors(node, currentNode) {
+            var ancestors = [];
+            var whileNode = node;
+            while (whileNode.parent) {
+                ancestors.unshift(whileNode);
+                whileNode = whileNode.parent;
+            }
+            return (ancestors.indexOf(currentNode) >= 0);
         }
 
         return {
             arc: arc,
             partition: partition,
             render: render,
-            centerSVG: centerSVG,
-            jsonRequestCB: jsonRequestCB
+            jsonRequestCB: jsonRequestCB,
+            getNodeAncestors: getNodeAncestors
         };
     }();
 
-    function renderSourcesPanel(sel, sources) {
-        var $panel = $(sel),
-            i = 0,
-            sourcesLen = sources.length;
-
-        for ( i; i < sourcesLen; i++) {
-            var source = sources[i],
-                sourceRef = "<span>[" + (i + 1) + "] </span>",
-                lastUpdated = "<span> - Retrieved " + source.lastUpdated + "</span",
-                link = "<div><div class='spacer-xs'></div><a href='" + source.link + "'>" + source.link + "</a></div>";
-
-            $panel.append("<li class='text-xs'>" + sourceRef + lastUpdated + link + "<div class='spacer-md'></div></li>");
-        }
-    }
-
-    d3Partitions.render();
+    D3Partitions.render();
 
 });
