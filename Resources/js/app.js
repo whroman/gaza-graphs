edaece96eca739c2fcfb91adcb859cf3baf3d9d0
whroman <me@walterroman.com>
2014-08-19 17:55:10 -0700

    Refactored JS structure

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,50 +1,50 @@
 $(function() {
 
-    var Templates = {
-        renderPrimaryTitle: function(node) {
-            var i = 0;
-            var sourcesLen = node.sources.length;
-            var html = node.title;
+var Templates = {
+    renderPrimaryTitle: function(node) {
+        var i = 0;
+        var sourcesLen = node.sources.length;
+        var html = node.title;
 
-            for ( i; i < sourcesLen; i++) {
-                var source = node.sources[i];
-                var sourceRef = node.parent.title + "-" + source;
-                html += "<a href='" + "#" + sourceRef + "' class='superscript text-green'>[" + source + "]</a>";
-            }
-
-            return html;
-        },
-        renderSecondary: function(node) {
-            return html;
-        },
-        renderSource: function(source) {
-            var index = source.i + 1;
-            var sourceRef = source.title + "-" + index; 
-            var lastUpdated = "<span> - Retrieved " + source.lastUpdated + "</span";
-            var link = "<div><div class='spacer-xs'></div><a href='" + source.link + "'>" + source.link + "</a></div>";
-            var html = "<li id='" + sourceRef + "'class='text-xs source'>" + "<span>[" + index + "]</span>" + lastUpdated + link + "<div class='spacer-md'></div></li>";
-
-            return html;
-        },
-        renderSourcesPanel: function($panel, json) {
-            var i = 0,
-                sourcesLen = json.sources.length;
-
-            for ( i; i < sourcesLen; i++) {
-                var source = json.sources[i];
-                source.i = i;
-                source.title = json.title;
-                var html = Templates.renderSource(source);
-
-                $panel.append(html);
-            }
-            events.hightlightSourceByHash();
+        for ( i; i < sourcesLen; i++) {
+            var source = node.sources[i];
+            var sourceRef = node.parent.title + "-" + source;
+            html += "<a href='" + "#" + sourceRef + "' class='superscript text-green'>[" + source + "]</a>";
         }
-    };
 
-    var events = {
-        onPathMouseover: function(d, graph) {
-            graph.paths
+        return html;
+    },
+    renderSecondary: function(node) {
+        return html;
+    },
+    renderSource: function(source) {
+        var index = source.i + 1;
+        var sourceRef = source.title + "-" + index; 
+        var lastUpdated = "<span> - Retrieved " + source.lastUpdated + "</span";
+        var link = "<div><div class='spacer-xs'></div><a href='" + source.link + "'>" + source.link + "</a></div>";
+        var html = "<li id='" + sourceRef + "'class='text-xs source'>" + "<span>[" + index + "]</span>" + lastUpdated + link + "<div class='spacer-md'></div></li>";
+
+        return html;
+    },
+    renderSourcesPanel: function($panel, json) {
+        var i = 0;
+        var sourcesLen = json.sources.length;
+
+        for ( i; i < sourcesLen; i++) {
+            var source = json.sources[i];
+            source.i = i;
+            source.title = json.title;
+            var html = Templates.renderSource(source);
+
+            $panel.append(html);
+        }
+        events.hightlightSourceByHash();
+    }
+};
+
+var events = {
+    onPathMouseover: function(d, graph) {
+        graph.paths
             // Fade all fill colors
             .classed("faded", true)
             // Highlight only those that are an ancestor of the current segment
@@ -54,166 +54,182 @@ $(function() {
             })
             .classed("faded", false);
 
-            graph.info.update(d);
-        },
-        hightlightSourceByHash: function() {
-            var hash = window.location.hash;
-            var hightlightClass = "text-green";
-            $(".source").removeClass(hightlightClass);
-            if (hash) {
-                $(window.location.hash).addClass(hightlightClass);
-            }
+        graph.info.update(d);
+    },
+    hightlightSourceByHash: function() {
+        var hash = window.location.hash;
+        var hightlightClass = "text-green";
+        // Remove text color from all sources
+        $(".source").removeClass(hightlightClass);
+
+        // If `hash` exists, highlight corresponding element
+        if (hash) {
+            $(window.location.hash).addClass(hightlightClass);
         }
-    };
+    }
+};
 
-    function InfoPanel ($wrapper) {
-        var $panel = $wrapper.find("[info]"),
-            $primary = $wrapper.find("[primary]"),
-            $primaryTitle = $primary.find("[title]"),
-            $primaryValue = $primary.find("[value]"),
-            $secondary = $wrapper.find("[secondary]"),
-            $secondaryTitle = $secondary.find("[title]"),
-            $secondaryValue = $secondary.find("[value]"),
-            classHidden = "hidden";
+function InfoPanel ($wrapper) {
+    var $panel = $wrapper.find("[info]"),
+        $primary = $wrapper.find("[primary]"),
+        $primaryTitle = $primary.find("[title]"),
+        $primaryValue = $primary.find("[value]"),
+        $secondary = $wrapper.find("[secondary]"),
+        $secondaryTitle = $secondary.find("[title]"),
+        $secondaryValue = $secondary.find("[value]"),
+        classHidden = "hidden";
 
-        function updatePrimary(node) {
-            var titleHTML = Templates.renderPrimaryTitle(node);
-            $primaryTitle.html(titleHTML);  
-            $primaryValue.text(node.value);
-        }
-
-        function updateSecondary(node) {
-            $secondary.removeClass(classHidden);
-            $secondaryTitle.text(node.title);
-            $secondaryValue.text(node.value);
-        }
-
-        function update(d) {
-            if (d.depth === 1) {
-                $secondary.addClass(classHidden);
-                updatePrimary(d);
-            } else if (d.depth === 2) {
-                updatePrimary(d.parent);
-                updateSecondary(d);           
-            }
-        }
-
-        function hide() {
-            $panel.addClass(classHidden);
-        }
-
-        return {
-            update: update
-        };
+    function updatePrimary(node) {
+        var titleHTML = Templates.renderPrimaryTitle(node);
+        $primaryTitle.html(titleHTML);  
+        $primaryValue.text(node.value);
     }
 
-    var D3Partitions = function() {
-        var graphs = {},
-            width = 650,
-            height = 650,
-            radius = Math.min(width, height) / 2,
-            svgCenter = "translate(" + width / 2 + "," + height / 2 + ")";
+    function updateSecondary(node) {
+        $secondary.removeClass(classHidden);
+        $secondaryTitle.text(node.title);
+        $secondaryValue.text(node.value);
+    }
 
-        var partition = d3.layout.partition()
-            .size([2 * Math.PI, radius * radius]);
-
-        var arc = d3.svg.arc()
-            .startAngle(function(d) { return d.x; })
-            .endAngle(function(d) { return d.x + d.dx; })
-            .innerRadius(function(d) { return Math.sqrt(d.y); })
-            .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
-        function jsonRequestCB(error, json) {
-            var key;
-            if (!error) {
-                key = json.title + ".json";
-                renderGraph(json, graphs[key]);
-                attachGraphEvents(graphs[key]);
-                Templates.renderSourcesPanel(graphs[key].$sources, json);                
-            }
+    function update(d) {
+        if (d.depth === 1) {
+            $secondary.addClass(classHidden);
+            updatePrimary(d);
+        } else if (d.depth === 2) {
+            updatePrimary(d.parent);
+            updateSecondary(d);           
         }
+    }
 
-        function renderGraph(json, graph) {
-            graph.paths = graph.svg.datum(json).selectAll("path")
-                .data(D3Partitions.partition.nodes)
-                .enter()
-                .append("path");
+    function hide() {
+        $panel.addClass(classHidden);
+    }
 
-            graph.paths
-                .attr("display", function(d) {
-                    return d.depth === 0 ? "none" : null; // hide inner ring
-                })
-                .attr("class", function(d) { 
-                    if (d.depth > 1) {
-                        return d.title + " depth-" + d.depth; 
-                    }
-                })
-                .attr("d", D3Partitions.arc);
+    return {
+        update: update
+    };
+}
 
-            // Load graph with one selected <path>
-            var data = d3.select("path.depth-2")[0][0].__data__;
-            events.onPathMouseover(data, graph);
-        }
+var D3Partitions = function() {
+    var graphs = {},
+        width = 650,
+        height = 650,
+        radius = Math.min(width, height) / 2,
+        svgCenter = "translate(" + width / 2 + "," + height / 2 + ")";
 
-        function attachGraphEvents(graph) {
-            graph.paths.on("mouseover", function(d) {
-                events.onPathMouseover(d, graph);
-            });  
-        }
+    var partition = d3.layout.partition()
+        .size([2 * Math.PI, radius * radius]);
 
-        function render() {
-            var elements = $("[d3-partition-graph]"),
-                i = 0,
-                elementsLen = elements.length;
+    var arc = d3.svg.arc()
+        .startAngle(function(d) { return d.x; })
+        .endAngle(function(d) { return d.x + d.dx; })
+        .innerRadius(function(d) { return Math.sqrt(d.y); })
+        .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
 
-            for ( i; i < elementsLen; i++ ) {
-                var element = d3.select(elements[i]);
-                var jsonPath = element.attr("data-source");
-                var svg = element.select("[graph]")
-                        .append("svg")
-                        .attr("width", width)
-                        .attr("height", height)
-                        .append("g")
-                        .attr("transform", svgCenter);
-                var $sources = $("[data-source='" + jsonPath + "']");
+    function jsonRequestCB(json) {
+        var key = json.title + ".json";
+        var graph = D3Partitions.graphs[key];
+        graph.json = json;
+        renderGraph(graph);
+        attachGraphEvents(graph);
+        Templates.renderSourcesPanel(graph.$sources, graph.json);
+    }
 
-                var graph = {
-                    wrapper: element,
-                    jsonPath: jsonPath,
-                    svg: svg,
-                    $sources: $sources,
-                    info: new InfoPanel($(elements[i]))
-                };
+    function renderGraph(graph) {
+        // Render <path> els
+        graph.paths = graph.svg.datum(graph.json).selectAll("path")
+            .data(D3Partitions.partition.nodes)
+            .enter()
+            .append("path");
 
-                graphs[jsonPath] = graph;
+        // Style <path> els
+        graph.paths
+            .attr("display", function(d) {
+                return d.depth === 0 ? "none" : null; // hide inner ring
+            })
+            .attr("class", function(d) { 
+                if (d.depth > 1) {
+                    return d.title + " depth-" + d.depth; 
+                }
+            })
+            .attr("d", D3Partitions.arc);
 
-                d3.json(graph.jsonPath, function(error, json) {
-                    D3Partitions.jsonRequestCB(error, json);
-                });
-            }
-        }
+        // Load graph with one highlighted <path>
+        var data = d3.select("path.depth-2")[0][0].__data__;
+        events.onPathMouseover(data, graph);
+    }
 
-        function getNodeAncestors(node, currentNode) {
-            var ancestors = [];
-            var whileNode = node;
-            while (whileNode.parent) {
-                ancestors.unshift(whileNode);
-                whileNode = whileNode.parent;
-            }
-            return (ancestors.indexOf(currentNode) >= 0);
-        }
+    function attachGraphEvents(graph) {
+        graph.paths.on("mouseover", function(d) {
+            events.onPathMouseover(d, graph);
+        });  
 
-        return {
-            arc: arc,
-            partition: partition,
-            render: render,
-            jsonRequestCB: jsonRequestCB,
-            getNodeAncestors: getNodeAncestors
+        return this;
+    }
+
+    function createGraphObject(el) {
+        var $el = $(el);
+        var wrapper = d3.select(el);
+        var jsonPath = wrapper.attr("data-source");
+        var $sources = $("[data-source='" + jsonPath + "']");
+        var svg = wrapper.select("[graph]")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", svgCenter);
+
+        var graph = {
+            wrapper: wrapper,
+            json: null, // To be filled by AJAX request
+            jsonPath: jsonPath,
+            svg: svg,
+            $sources: $sources,
+            info: new InfoPanel($el)
         };
-    }();
 
-    D3Partitions.render();
+        return graph;
+    }
 
+    function render(sel) {
+        var elements = $(sel),
+            i = 0,
+            elementsLen = elements.length;
 
-    $(window).on("hashchange", events.hightlightSourceByHash);
+        for ( i; i < elementsLen; i++ ) {
+            var el = elements[i];
+            var graph = createGraphObject(el);
+
+            D3Partitions.graphs[graph.jsonPath] = graph;
+
+            $.getJSON(graph.jsonPath)
+            .success(jsonRequestCB);
+        }
+        return this;
+    }
+
+    function getNodeAncestors(node, currentNode) {
+        var ancestors = [];
+        var whileNode = node;
+        while (whileNode.parent) {
+            ancestors.unshift(whileNode);
+            whileNode = whileNode.parent;
+        }
+        return (ancestors.indexOf(currentNode) >= 0);
+    }
+
+    return {
+        graphs: graphs,
+        arc: arc,
+        partition: partition,
+        render: render,
+        jsonRequestCB: jsonRequestCB,
+        getNodeAncestors: getNodeAncestors
+    };
+}();
+
+D3Partitions.render("[d3-partition-graph]");
+
+$(window).on("hashchange", events.hightlightSourceByHash);
+
 });
